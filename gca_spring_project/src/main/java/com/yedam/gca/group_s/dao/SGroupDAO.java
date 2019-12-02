1bb3549f36d6b7ad52b5c2652e18b52110072116
Jey <Jey@naver.com>
2019-12-02 18:00:21 +0900

    은영 s_groupMapper 수정, s_groupdao 약간 수정

--- gca_spring_project/src/main/java/com/yedam/gca/group_s/dao/SGroupDAO.java
+++ gca_spring_project/src/main/java/com/yedam/gca/group_s/dao/SGroupDAO.java
@@ -1,5 +1,18 @@
 package com.yedam.gca.group_s.dao;
 
-public class SGroupDAO {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import com.yedam.gca.group_s.vo.SGroupVO;
+
+@Repository("sGroupDAO")
+public class SGroupDAO {
+	
+	@Autowired
+	SqlSessionTemplate mybatis;
+	
+	public SGroupVO getRoomInfo(SGroupVO vo) {
+		return mybatis.selectOne("SGroupDAO.getRoomInfo", vo);
+	}
 }
