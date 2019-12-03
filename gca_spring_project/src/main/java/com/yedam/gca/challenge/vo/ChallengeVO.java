50da262e8b5e99d8bba4d897179f6f65ec84c9ae
devsurimlee <devsurimlee@gmail.com>
2019-12-03 15:31:11 +0900

    수림 챌린지.jsp 값넣는중

--- gca_spring_project/src/main/java/com/yedam/gca/challenge/vo/ChallengeVO.java
+++ gca_spring_project/src/main/java/com/yedam/gca/challenge/vo/ChallengeVO.java
@@ -4,17 +4,17 @@ import java.sql.Date;
 
 import lombok.Data;
 
-@Data
+@Data  //수림 수정
 public class ChallengeVO {
-
-	private int cl_number;			// *PK* // 챌린지 번호
+	
+	private int cl_num;			// *PK* // 챌린지 번호
 	private String cl_status;				// 챌린지 구분 (기본/스페셜)
 	private String cl_name;					// 챌린지명
-	private String cl_contents;				// 챌린지 내용
-	private String cl_sports;				// 챌린지 종목
-	private int cl_count;					// 챌린지 성공횟수
-	private Date cl_start;					// 시작 날짜
-	private Date cl_end;					// 마감 날짜
+	private String cl_content;				// 챌린지 내용
+	private String sport1_cd;				// 챌린지 종목
+	private int cl_cnt;					// 챌린지 성공횟수
+	private Date cl_start_dttm;					// 시작 날짜
+	private Date cl_end_dttm;					// 마감 날짜
 	private int cl_score;					// 리워드 점수
 	
 }
