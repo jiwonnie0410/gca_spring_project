131dce80df799ed67ce28b5a48aee1c697545752
User <User@YD01-17>
2019-12-09 18:17:34 +0900

    지원 통계랑 챌린지 구현 중

--- gca_spring_project/src/main/java/com/yedam/gca/admin/controller/AdminController.java
+++ gca_spring_project/src/main/java/com/yedam/gca/admin/controller/AdminController.java
@@ -18,8 +18,9 @@ public class AdminController {
 // 1-1챌린지 관리 페이지		1-2챌린지 등록
 // 2매출 페이지
 // 3-1통계 페이지 	3-2반짝 운동별 통계 	3-3동호회 매치 운동별 통계 	 3-4연령대 및 운동(반짝)성별 통계 	3-5지역별 반짝 운동 통계
-	
-	@Autowired AdminService adminService;
+
+	@Autowired
+	AdminService adminService;
 
 	// 지원
 	// 1-1. 챌린지 관리 페이지
@@ -27,7 +28,7 @@ public class AdminController {
 	public String adminChallenge() {
 		return "/admin/admin_challenge_list";
 	}
-	
+
 	// 1-2. 챌린지 등록
 	@ResponseBody
 	@RequestMapping("/ajax/createChallenge")
@@ -59,20 +60,44 @@ public class AdminController {
 	// 3-3. 통계 -> 동호회 매치 운동별 통계
 	@ResponseBody
 	@RequestMapping("/ajax/chart/bgroup")
-	public List<Map<String, Object>> chartBgroup(){
+	public List<Map<String, Object>> chartBgroup() {
 		return adminService.chartBgroup();
 	}
 
 	// 3-4. 통계 -> 연령대 및 운동(반짝)성별 통계
+	@ResponseBody
+	@RequestMapping("/ajax/chart/gender")
+	public List<Map<String, Object>> chartGender() {
+		return adminService.chartGender("G01", 20, 29);
+	}
 
 	// 3-5. 통계 -> 지역별 반짝 운동 통계
 	@ResponseBody
 	@RequestMapping("/ajax/chart/city")
-	public List<Map<String, Object>> chartCity(){
-		System.out.println("대구 12월: " + adminService.chartCity(12, "대구"));
-		System.out.println("대구 11월: " + adminService.chartCity(11, "대구"));
-		
-		return adminService.chartCity(12, "대구");
+	public List<Map<String, Object>> chartCity() {
+//		Calendar calendar = new GregorianCalendar(Locale.KOREA);
+//		String nowMonth = Integer.toString(calendar.get(Calendar.MONTH) + 1); 	// 현재 달
+//		String oneMonth, twoMonth, threeMonth;									// 한달전, 두달전, 세달전
+//		
+//		// 만약 구하려는 달이 10월 전이라면 쿼리에는 09 08 07월로 들어가야 하기 때문에 문자열로 앞에 0을 붙여줌
+//		// 현재 달
+//		if((calendar.get(Calendar.MONTH)+1) < 10) {	nowMonth = "0".concat(Integer.toString(calendar.get(Calendar.MONTH) + 1)); }
+//		else nowMonth = Integer.toString(calendar.get(Calendar.MONTH) + 1);
+//		// 한달 전
+//		if((calendar.get(Calendar.MONTH)) < 10) {	oneMonth = "0".concat(Integer.toString(calendar.get(Calendar.MONTH))); }
+//		else oneMonth = Integer.toString(calendar.get(Calendar.MONTH));
+//		// 두 달 전
+//		if((calendar.get(Calendar.MONTH)-1) < 10) {	twoMonth = "0".concat(Integer.toString(calendar.get(Calendar.MONTH) - 1)); }
+//		else twoMonth = Integer.toString(calendar.get(Calendar.MONTH) - 1);
+//		// 세 달 전
+//		if((calendar.get(Calendar.MONTH)-2) < 10) {	threeMonth = "0".concat(Integer.toString(calendar.get(Calendar.MONTH) - 2)); }
+//		else threeMonth = Integer.toString(calendar.get(Calendar.MONTH) - 2);
+
+//		for(int i=(Integer.parseInt(startMonth)); i<=Integer.parseInt(nowMonth); i++) {
+//			adminService.chartCity(startMonth, "대구");
+//		}
+
+		return adminService.chartCity("12", "대구");
 	}
 
 }
