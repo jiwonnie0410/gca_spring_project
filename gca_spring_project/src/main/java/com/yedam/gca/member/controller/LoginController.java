64dc721462fa8f7020a8e464e775ed095aa25d41
User <User@YD01-17>
2019-12-17 15:03:49 +0900

    지원 멤버 테이블 수정

--- gca_spring_project/src/main/java/com/yedam/gca/member/controller/LoginController.java
+++ gca_spring_project/src/main/java/com/yedam/gca/member/controller/LoginController.java
@@ -32,17 +32,10 @@ public class LoginController {
 
 	@Autowired
 	MemberService memberService;
-
-	// 지원
-	private NaverLoginBO naverLoginBO;
 	private String apiResult = null;
 
-	@Autowired
-	private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
-		this.naverLoginBO = naverLoginBO;
-	}
-	
-//	// 첫 로그인 페이지 (홈페이지 열자마자 보이는 화면)
+	// 지원
+	//	// 첫 로그인 페이지 (홈페이지 열자마자 보이는 화면)
 //	@RequestMapping(value = "/", method = RequestMethod.GET)
 //	public String home(Locale locale, Model model) {
 //		return "/notiles/member/login";
@@ -52,12 +45,11 @@ public class LoginController {
 	@RequestMapping(value = "/", method = { RequestMethod.GET, RequestMethod.POST })
 	public String home(Locale locale, Model model, HttpSession session) {
 		// 네이버아이디로 인증 URL을 생성하기 위하여 naverLoginBO클래스의 getAuthorizationUrl메소드 호출
-		String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
-		// https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=sE***************&
-		// redirect_uri=http%3A%2F%2F211.63.89.90%3A8090%2Flogin_project%2Fcallback&state=e68c269c-5ba9-4c31-85da-54c16c658125
+		MembersVO vo = new MembersVO();
+		String naverAuthUrl = vo.getAuthorizationUrl(session);
 		System.out.println("네이버:" + naverAuthUrl);
-		// 네이버
 		model.addAttribute("url", naverAuthUrl);
+		System.out.println("네이버 authurl::::: " + naverAuthUrl);
 		
 		return "/notiles/member/login";
 	}
@@ -71,16 +63,20 @@ public class LoginController {
 	// 네이버 아이디로 로그인 성공 시에 돌아올 페이지 -> 반짝 방 리스트로 감
 	@RequestMapping(value = "/login/naverCallback", method = { RequestMethod.GET, RequestMethod.POST })
 	public String naverCallback(Model model, @RequestParam String code, @RequestParam String state, HttpSession session) throws IOException, ParseException {
-		System.out.println("여기는 callback");
+		//SecurityContextHolder.getContext()
+		
+		MembersVO vo = new MembersVO();
+		System.out.println("***************************************여기는 callback************************************************");
 		OAuth2AccessToken oauthToken;
-		oauthToken = naverLoginBO.getAccessToken(session, code, state);
+		oauthToken = vo.getAccessToken(session, code, state);
 		
 		// 1. 로그인 사용자 정보를 읽어온다.
-		apiResult = naverLoginBO.getUserProfile(oauthToken); // String형식의 json데이터
+		apiResult = vo.getUserProfile(oauthToken); // String형식의 json데이터
 		/**
 		 * apiResult json 구조 {"resultcode":"00", "message":"success",
 		 * "response":{"id":"33666449","nickname":"shinn****","age":"20-29","gender":"M","email":"sh@naver.com","name":"\uc2e0\ubc94\ud638"}}
 		 **/
+		System.out.println("여기는 콜백의 apiResult::: " + apiResult);
 		
 		// 2. String형식인 apiResult를 json형태로 바꿈
 		JSONParser parser = new JSONParser();
@@ -91,11 +87,11 @@ public class LoginController {
 		// Top레벨 단계 _response 파싱
 		JSONObject response_obj = (JSONObject) jsonObj.get("response");
 		// response의 nickname값 파싱
-		String nickname = (String) response_obj.get("nickname");
-		System.out.println(nickname);
+		String id = (String) response_obj.get("id");
+		System.out.println(id);
 		
 		// 4. 파싱 닉네임 세션으로 저장
-		session.setAttribute("sessionId", nickname); // 세션 생성
+		session.setAttribute("naverId", id); // 세션 생성
 		model.addAttribute("result", apiResult);
 		
 		return "/notiles/member/callback";
