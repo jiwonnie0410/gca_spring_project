5cddb61f5967f4301e22d6780ff07ac25b168d90
devsurimlee <devsurimlee@gmail.com>
2019-12-18 15:56:43 +0900

    수림 VO중 상속받은 VO만 어노테이션 추가 @EqualsAndHashCode(callSuper=false)

--- gca_spring_project/src/main/java/com/yedam/gca/board/vo/BoardSearchVO.java
+++ gca_spring_project/src/main/java/com/yedam/gca/board/vo/BoardSearchVO.java
@@ -1,10 +1,12 @@
 package com.yedam.gca.board.vo;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter @ToString
+@EqualsAndHashCode(callSuper=false)
 public class BoardSearchVO extends QnaBoardVO {
 	private String keyword;
 	private String searchCondition;
