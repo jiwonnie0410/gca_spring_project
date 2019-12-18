745a874276a4d76b1a386f83e184e140552dfa86
devsurimlee <devsurimlee@gmail.com>
2019-12-18 15:51:38 +0900

    수림 빌드용 젠킨스

--- gca_spring_project/src/main/java/com/yedam/gca/board/vo/BoardSearchVO.java
+++ gca_spring_project/src/main/java/com/yedam/gca/board/vo/BoardSearchVO.java
@@ -1,8 +1,10 @@
 package com.yedam.gca.board.vo;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter @Setter @ToString
 public class BoardSearchVO extends QnaBoardVO {
 	private String keyword;
 	private String searchCondition;
